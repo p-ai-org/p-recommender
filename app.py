47b7fc121c16a4f4becde460f3be5de268416584
saatvikkher <saatvikkher1@gmail.com>
2022-10-14 23:26:50 -0700

    get_recommendations function

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+def get_recommendations(title):
+    return None
+
+
 @app.route('/')
 
 def main():
-    return "Hello Claremont!"
+    return "Hello Claremont! Welcome to p-recommender."
 
 if __name__ == '__main__':
     app.run()
