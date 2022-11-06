46c1499f75a32d4432be9f33e8ac093e61ac453d
majonajas <114883084+majonajas@users.noreply.github.com>
2022-11-06 14:26:56 -0800

    update

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
-from flask import Flask
+
+
+from flask import Flask, render_template, request
+import pandas as pd
+
+
 app = Flask(__name__)
 
 
-def get_recommendations(title):
-    return None
+#function to get recs
+def get_recs(course_title):
+    result = [course_title for i in range(1, 10)]
+    result = pd.DataFrame(result, columns = "Recs")
+    return result
 
 
 @app.route('/')
+def index():
+    return render_template()
+
+
 
 def main():
     return "Hello Claremont! Welcome to p-recommender."
 
 if __name__ == '__main__':
     app.run()
+
+
