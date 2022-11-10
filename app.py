c9a9dba977cdea02d8f3bcdfd7d07668d34bf10f
sophiaaristuben <sara2020@mymail.pomona.edu>
2022-11-09 19:08:06 -0800

    Added to app.py
    App.py and style sheet submit button

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request 
+import pandas as pd
+
+
 app = Flask(__name__)
 
+#function to get recs
+def get_recs(course_title):
+    # global result
+    result = [course_title for i in range(1, 10)]
+    result = pd.DataFrame(result)
+    return result
 
-def get_recommendations(title):
-    return None
-
-
+#index file routing
 @app.route('/')
 
-def main():
-    return "Hello Claremont! Welcome to p-recommender."
+def index():
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run()
+# results file routing
+@app.route('/rec', methods=['POST'])
+def rec_page():
+    coursename = request.form["coursename"]
+    #result = get_recs(coursename)
+    #tables = [result.to_html(classes='data')],
+    return render_template("results.html", coursename = coursename)
+
+# def main():
+#     return "Hello Claremont! Welcome to p-recommender."
+
+# if __name__ == '__main__':
+#     app.run()
