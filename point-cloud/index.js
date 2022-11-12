a7de74860594bfb543ed362253a789a25c542e35
Samson Zhang <wwsamson@yahoo.com>
2022-11-12 11:45:07 -0800

    slight update performance improvements

--- point-cloud/index.js
+++ point-cloud/index.js
@@ -8,7 +8,7 @@ svg
     .attr("height", h)
     .attr("viewbox", [0, 0, w, h]);
 
-svg.append("rect").attr("width", w).attr("height", h).attr("fill", "#222");
+const bg = svg.append("rect").attr("width", w).attr("height", h).attr("fill", "#222");
 
 d3.json("alex_courses.json").then(courses => {
     d3.csv("alex_connections.csv").then(links => {
@@ -42,12 +42,6 @@ d3.json("alex_courses.json").then(courses => {
 
             loading.remove();
 
-            drawGraph();
-        });
-
-        function drawGraph(highlightId) {
-            console.log(highlightId);
-
             const links = g.selectAll("line.link")
                 .data(linksProcessed)
                 .join("line")
@@ -59,7 +53,7 @@ d3.json("alex_courses.json").then(courses => {
                 .attr("stroke", "red")
                 // .attr("stroke-width", d => Math.sqrt(d.strength) * 4)
                 .attr("stroke-width", 2)
-                .style("opacity", d => (!highlightId || d.source.id === highlightId || d.target.id === highlightId) ? d.strength : d.strength * 0.1);
+                .style("opacity", d => d.strength);
 
             const nodes = g.selectAll("circle.course")
                 .data(coursesProcessed)
@@ -67,13 +61,11 @@ d3.json("alex_courses.json").then(courses => {
                 .attr("class", "course")
                 .attr("cx", d => d.x)
                 .attr("cy", d => d.y)
-                .attr("r", d => d.id === highlightId ? 10 : 3)
-                .attr("fill", d => d.id === highlightId ? "blue" : "red")
+                .attr("r", d => 3)
+                .attr("fill", d => "red")
                 .on("click", (e, d) => {
-                    console.log(d.id);
-                    drawGraph(d.id);
-                })
-                .style("opacity", d => (!highlightId || d.id === highlightId || linksProcessed.filter(x => x.target.id === highlightId || x.source.id === highlightId).map(x => (x.target.id === highlightId) ? x.source.id : x.target.id).includes(d.id)) ? 1 : 0.1);
+                    updateGraph(d.id);
+                });
 
             const text = g.selectAll("text.courseLabel")
                 .data(coursesProcessed)
@@ -85,9 +77,23 @@ d3.json("alex_courses.json").then(courses => {
                 .text(d => d.title)
                 .attr("font-size", "6px")
                 .style("opacity", "0.75")
-                .attr("fill", "red")
-                .style("opacity", d => (!highlightId || d.id === highlightId || linksProcessed.filter(x => x.target.id === highlightId || x.source.id === highlightId).map(x => (x.target.id === highlightId) ? x.source.id : x.target.id).includes(d.id)) ? 1 : 0.1);
-        }
+                .attr("fill", "red");
+
+            bg.on("click", updateGraph());
+
+            function updateGraph(highlightId) {
+                links
+                    .style("opacity", d => (!highlightId || d.source.id === highlightId || d.target.id === highlightId) ? d.strength : d.strength * 0.1);
+
+                nodes
+                    .attr("r", d => d.id === highlightId ? 10 : 3)
+                    .attr("fill", d => d.id === highlightId ? "blue" : "red")
+                    .style("opacity", d => (!highlightId || d.id === highlightId || linksProcessed.filter(x => x.target.id === highlightId || x.source.id === highlightId).map(x => (x.target.id === highlightId) ? x.source.id : x.target.id).includes(d.id)) ? 1 : 0.1);
+
+                text
+                    .style("opacity", d => (!highlightId || d.id === highlightId || linksProcessed.filter(x => x.target.id === highlightId || x.source.id === highlightId).map(x => (x.target.id === highlightId) ? x.source.id : x.target.id).includes(d.id)) ? 1 : 0.1);
+            }
+        });
 
         svg.call(d3.zoom()
             .translateExtent([[-10 * w, -10 * h], [10 * w, 10 * h]])
