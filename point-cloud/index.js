b27c98b3ff689650165edc5ad09a889089e3eaca
Samson Zhang <wwsamson@yahoo.com>
2022-11-12 02:21:27 -0800

    add point cloud live data

--- point-cloud/index.js
+++ point-cloud/index.js
@@ -5,34 +5,46 @@ const h = 800;
 
 svg
     .attr("width", w)
-    .attr("height", h);
+    .attr("height", h)
+    .attr("viewbox", [0, 0, w, h]);
 
-d3.csv("courses_temp.csv").then(courses => {
-    d3.csv("links_temp.csv").then(links => {
-        const linksProcessed = links.map(d => ({
-            source: courses.findIndex(x => x.id === d.id1),
-            target: courses.findIndex(x => x.id === d.id2),
-            strength: d.strength,
+d3.json("alex_courses.json").then(courses => {
+    d3.csv("alex_connections.csv").then(links => {
+        const g = svg.append("g");
+
+        const coursesProcessed = Object.keys(courses.title).map(d => ({
+            id: courses.identifier[d],
+            title: courses.title[d],
+            description: courses.description[d],
         }));
 
-        const force = d3.forceSimulation(courses)
-            .force("charge", d3.forceManyBody().strength(-200))
+        const linksProcessed = links.map(d => ({
+            source: +d.node1,
+            target: +d.node2,
+            strength: +d.score,
+        })).sort((a, b) => b.strength - a.strength).splice(0, 5000);
+
+        console.log(linksProcessed);
+
+        const force = d3.forceSimulation(coursesProcessed)
+            .force("charge", d3.forceManyBody().strength(-10))
             .force("link", d3.forceLink(linksProcessed).strength(d => d.strength))
             .force("center", d3.forceCenter(w/2, h/2))
-            .force("collision", d3.forceCollide().radius(10))
+            // .force("collision", d3.forceCollide().radius(5))
             .on("tick", tick);
 
-        function tick() {
-            const nodes = svg.selectAll("circle.course")
-                .data(courses)
-                .join("circle")
-                .attr("class", "course")
-                .attr("r", 10)
-                .attr("cx", d => d.x)
-                .attr("cy", d => d.y)
-                .attr("fill", "red");
+        const tooltip = d3.select("body")
+            .append("div")
+            .attr("id", "tooltip")
+            .style("position", "fixed")
+            .style("background-color", "white")
+            .style("padding", 8)
+            .style("border", "1px solid black")
+            .style("top", 0)
+            .style("left", 0);
 
-            const links = svg.selectAll("line.link")
+        function tick() {
+            const links = g.selectAll("line.link")
                 .data(linksProcessed)
                 .join("line")
                 .attr("class", "link")
@@ -41,8 +53,38 @@ d3.csv("courses_temp.csv").then(courses => {
                 .attr("x2", d => d.target.x)
                 .attr("y2", d => d.target.y)
                 .attr("stroke", "red")
-                .attr("stroke-width", d => Math.sqrt(d.strength) * 4)
-                // .style("opacity", d => d.strength);
+                // .attr("stroke-width", d => Math.sqrt(d.strength) * 4)
+                .attr("stroke-width", 2)
+                .style("opacity", d => d.strength);
+
+            const nodes = g.selectAll("circle.course")
+                .data(coursesProcessed)
+                .join("circle")
+                .attr("class", "course")
+                .attr("cx", d => d.x)
+                .attr("cy", d => d.y)
+                .attr("r", 3)
+                .attr("fill", "blue");
+
+            const text = g.selectAll("text.courseLabel")
+                .data(coursesProcessed)
+                .join("text")
+                .attr("class", "courseLabel")
+                .attr("x", d => d.x)
+                .attr("y", d => d.y)
+                .text(d => d.title)
+                .attr("font-size", "4px")
+                .style("opacity", "0.5");
+        }
+
+        svg.call(d3.zoom()
+            .translateExtent([[-10 * w, -10 * h], [10 * w, 10 * h]])
+            .scaleExtent([0.1, 10])
+            .on("zoom", zoomed)
+        );
+
+        function zoomed({transform}) {
+            g.attr("transform", transform);
         }
     });
 })
